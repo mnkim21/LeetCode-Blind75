3711c93b251a6211861a5400b96cc45a349178db
MINA KIM <mnkim21@gmail.com>
2024-05-20 00:09:46 +0900

    Time: 44 ms (39.62%), Space: 44.5 MB (89.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        /*
-            for(
-                for()
-            )
+    public int[] twoSum(int[] nums, int target) { 
+        // ex) nums= [2,7,11,15] -> length = 4
+        //1.
+        // int[] result = new int[2];
 
-            EX >> nums = 0 1 2 3
-            0 -> 1 2 3 
-            1 -> 2 3
-            2 -> 3
-        */
-        int[] result = new int[2];
-        int len = nums.length;
-        for(int i = 0; i < len - 1; i++) {
-            for(int j = i + 1; j < len; j++){
-                if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
+        // for(int i = 0; i < nums.length - 1; i++) { // ex) 0 / 0 1 2
+        //     for(int y = i + 1; y < nums.length; y++) { // ex) 1 / 1 2 3
+        //         if(nums[i] + nums[y] == targe) {
+        //             result[0] = i;
+        //             result[1] = y;
+        //         }
+        //     }
+        // }
+        // return result;
+
+        // 2.
+        for(int i = 0; i < nums.length-1; i++) { // ex) 0 / 0 1 2
+            for(int y = i+1; y <nums.length; y++) { // ex) 1 / 1 2 3
+                if(nums[i] + nums[y] == target){
+                    return new int[] {i, y};
                 }
             }
         }
-        return result;
+        return null;
     }
 }
