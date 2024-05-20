ea81336c98895ad4e493c4a306897fd6c2e95d9c
MINA KIM <mnkim21@gmail.com>
2024-05-20 14:28:29 +0900

    Time: 10 ms (88.40%), Space: 59.6 MB (36.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // int first = nums[0];
+        // for(int i = 1; i < nums.length; i++) {
+        //     if(first == nums[i]){
+        //         return true;
+        //     } else {
+        //         first = nums[i];
+        //     }
+        // }
+        // return false;
 
-        int first = nums[0];
-        for(int i = 1; i < nums.length; i++) {
-            if(first == nums[i]){
+        Set<Integer> numsSet = new HashSet<>(); // SET = 중복허용x
+        for(int i = 0; i < nums.length; i++) {
+            if(numsSet.contains(nums[i])) {
                 return true;
             } else {
-                first = nums[i];
+                numsSet.add(nums[i]);
             }
         }
         return false;
