4b5c9069d891f32ae0ab2362ef19a0d044de53d7
MINA KIM <mnkim21@gmail.com>
2024-05-24 17:29:04 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (49.75%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,10 +1,26 @@
 class Solution {
     public int findMin(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] > nums[i+1]) {
-                return nums[i+1];
+        // 1.
+        // for(int i = 0; i < nums.length - 1; i++) {
+        //     if(nums[i] > nums[i+1]) {
+        //         return nums[i+1];
+        //     }
+        // }
+        // return nums[0];
+
+        //2.
+        // left -- mid -- right
+        // compare mid with right -> if mid is smaller than right -> right = mid / else left = mid + 1
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2; // 1) 0 + (0+6)/2 = 3; 2)4 + (6-4) / 2 = 5;
+
+            if(nums[mid] < nums[right]) { // nums[mid] = 7 mid[r] = 2 -> else left = 3 + 1 = 4
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return nums[0];
+        return nums[left];
     }
 }
