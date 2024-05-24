dbd84ab18d5ea4168fec8d184e89eb5cd1271dbe
MINA KIM <mnkim21@gmail.com>
2024-05-24 17:59:29 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (15.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,8 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == target) {
-                return i;
+        // 1.
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(nums[i] == target) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // 2.
+        // Ascending order, rotated
+        // left - mid - right
+        // if nums[l] <= target < mid) , else mid < target =< nums[r]
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
