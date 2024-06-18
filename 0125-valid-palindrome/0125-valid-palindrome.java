1af506047bb626820192b2a55fc6f36a34cc8a5d
MINA KIM <mnkim21@gmail.com>
2024-06-19 01:24:35 +0900

    Time: 24 ms (13.76%), Space: 45.3 MB (16.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,23 +7,28 @@ class Solution {
         isLetterOrDigit -> remove non-alphanumeric characters
          */
         
-        int left = 0, right = s.length() - 1;
-        while(left <= right){
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
+        // int left = 0, right = s.length() - 1;
+        // while(left <= right){
+        //     char leftChar = s.charAt(left);
+        //     char rightChar = s.charAt(right);
 
-            if(!Character.isLetterOrDigit(leftChar)){
-                left++;
-            } else if(!Character.isLetterOrDigit(rightChar)){
-                right--;
-            } else {
-                if(Character.toUpperCase(leftChar) != Character.toUpperCase(rightChar)){
-                    return false;
-                }
-                left++;
-                right--;
-            }
-        }
-        return true;
+        //     if(!Character.isLetterOrDigit(leftChar)){
+        //         left++;
+        //     } else if(!Character.isLetterOrDigit(rightChar)){
+        //         right--;
+        //     } else {
+        //         if(Character.toUpperCase(leftChar) != Character.toUpperCase(rightChar)){
+        //             return false;
+        //         }
+        //         left++;
+        //         right--;
+        //     }
+        // }
+        // return true;
+
+        // 2.
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        StringBuilder sb = new StringBuilder(s);
+        return s.equals(sb.reverse().toString());
     }
 }
